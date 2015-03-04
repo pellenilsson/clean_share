d37f5d4e304644a937e5afa52282cdc3ae29d708
Pelle Nilsson <pellenilsson@fastmail.fm>
2015-03-04 14:45:37 +0100

    Add logo images

--- js/clean_share.js
+++ js/clean_share.js
@@ -1,9 +1,11 @@
 var services = [
     {
+        ident: 'googleplus',
         name: 'Google+',
         url: 'https://plus.google.com/_/+1/confirm?url=@URL@',
     },
     {
+        ident: 'facebook',
         name: 'Facebook',
         url: 'http://www.facebook.com/sharer.php?u=@URL@&t=@TITLE@',
     },
@@ -14,12 +16,23 @@ function cleanShareLoad() {
     if (container == null)
         return;
 
+    image_base_url = '/img';
+    image_suffix = '-color.png';
+
+    var attr;
+    attr = container.getAttribute('image_base_url');
+    if( attr != null )
+        image_base_url = attr;
+    attr = container.getAttribute('image_suffix');
+    if( attr != null )
+        image_suffix = attr;
+
     var html = '';
     for (var i = 0; i < services.length; i++) {
         var url = services[i].url
             .replace('@URL@', encodeURIComponent(window.location.href))
             .replace('@TITLE@', encodeURIComponent(document.title));
-        html += '<a href="' + url + '">' + services[i].name + '</a>';
+        html += '<a href="' + url + '"><img alt="' + services[i].name + '" src="' + image_base_url + '/' + services[i].ident + image_suffix + '"></a>';
     }
 
     container.innerHTML = html;
